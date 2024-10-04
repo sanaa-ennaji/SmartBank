6ef9545e8fb40ab1490e26ee19c500b55ad939b8
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-04 14:57:08 +0100

    make histroric modefication dao

--- src/main/java/com/example/smartbank/DAO/DemandeCreditDAOImpl.java
+++ src/main/java/com/example/smartbank/DAO/DemandeCreditDAOImpl.java
@@ -1,11 +1,85 @@
 package com.example.smartbank.DAO;
 
-public class DemandeCreditDAOImpl {
+import com.example.smartbank.Entity.DemandeCredit;
+import com.example.smartbank.jpa.EntityManagerHelper;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 
+import java.util.List;
+
+public class DemandeCreditDAOImpl implements DemandeCreditDAO {
 
 
+    @Override
+    public DemandeCredit create(DemandeCredit demande) {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        EntityTransaction transaction = null;
+        try{
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(demande);
+            transaction.commit();
 
+        }catch(Exception  e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
 
+        }finally {
+            em.close();
+        }
+        return null;
+    }
 
+    @Override
+    public List<DemandeCredit> getAll() {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        List<DemandeCredit> demandes = null ;
+        try{
+            demandes = em.createQuery("SELECT d From DemandeCredit d", DemandeCredit.class).getResultList();
+        }finally {
+            em.close();
+        }
+        return demandes;
+    }
+
+    @Override
+    public DemandeCredit findById(long id) {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        DemandeCredit demande = null ;
+        try {
+            demande = em.find(DemandeCredit.class, id);
+        } finally {
+            em.close();
+        }
+        return demande;
+    }
+
+    @Override
+    public void updateStatus(DemandeCredit demande) {
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        EntityTransaction transaction = null ;
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.merge(demande);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
 }
