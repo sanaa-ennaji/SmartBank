0a4557e644e0319e1996b67c0b4a9839999da731
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-04 15:01:38 +0100

    service interface

--- src/main/java/com/example/smartbank/Service/DemandeCreditService.java
+++ src/main/java/com/example/smartbank/Service/DemandeCreditService.java
@@ -1,4 +1,12 @@
 package com.example.smartbank.Service;
 
+import com.example.smartbank.Entity.DemandeCredit;
+
+import java.util.List;
+
 public interface DemandeCreditService {
+     DemandeCredit create (double montant , int duree , String etat , String remarques);
+     List<DemandeCredit> getAll ();
+     DemandeCredit findById (long id);
+     void update (DemandeCredit demande);
 }
